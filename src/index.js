ba072d6b1e2a3aae540b548596632a2596d6c5b6
https://github.com/ayomidebajo <oluwashinabajo@gmail.com>
2019-11-21 12:50:24 +0100

    some

--- src/index.js
+++ src/index.js
@@ -7,17 +7,22 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig';
+
+//
+
+import { reduxFirestore,getFirestore } from 'redux-firestore';
+import { reactReduxFirebase,getFirebase } from 'react-redux-firebase';
+
+
+
 
 const store = createStore(rootReducer, 
     compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})
-    ),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
-    )
+        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+        reduxFirestore(fbConfig),
+        // reactReduxFirebase(fbConfig)
+        )
     );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
